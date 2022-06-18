a41c681a09547d877bb2c6cd1cce6c73ea5daf5d
Elliott Chong <elliottchong16@gmail.com>
2022-06-18 18:26:03 +0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const POPULATION_SIZE = 100;
+const LIFE_SPAN = 200;
+let count = 0;
+let population;
+let countP;
+let target;
+let obstacle;
+const MUTATION_RATE = 0.01;
+
+function setup() {
+  let cnv = createCanvas(500, 500);
+  countP = createP();
+  countP.parent(select("#container"));
+  population = new Population(POPULATION_SIZE, MUTATION_RATE);
+  cnv.parent(select("#canvas"));
+  target = createVector(width / 2, 50);
+  obstacle = {
+    x: width / 2,
+    y: height / 2 - 100,
+    width: width / 2 + 100,
+    height: 20,
+  };
+}
+
+function draw() {
+  background(0);
+  population.run(count);
+  count++;
+  if (count == LIFE_SPAN) {
+    count = 0;
+    population.calculateFitness();
+    // population.normalize();
+    population.reproduce();
+  }
+
+  countP.html(count);
+  rectMode(CENTER);
+  fill(255, 0, 0);
+  ellipse(target.x, target.y, 20, 20);
+  fill(255);
+  rect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+}
